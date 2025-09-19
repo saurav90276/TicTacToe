084f23d631eca3e3658ee8a5d0e1f353244f2126
Saurav Tomar <tomarsaurav10@gmail.com>
2025-09-19 13:15:30 +0530

    First commit

---
+++ TicTacToe.java
@@ -0,0 +1,184 @@
+package Project;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TicTacToe extends JFrame implements ActionListener{
+
+    public static void main(String[] args) {
+        TicTacToe tf = new TicTacToe();
+    }
+
+    public static int BOARD_SIZE = 3;
+    public static enum GameStatus{
+        Incomplete,XWins,OWins,Tie;
+    }
+    private JButton[][] buttons = new JButton[BOARD_SIZE][BOARD_SIZE];
+    boolean CrossTurn=true;
+    public TicTacToe(){
+        super.setTitle("TicTacToe");
+        super.setSize(600, 600);
+        GridLayout grid = new GridLayout(BOARD_SIZE, BOARD_SIZE);
+        super.setLayout(grid);
+        Font font = new Font("Comic Sans", 1, 150);
+        for(int row=0; row<BOARD_SIZE; row++){
+            for(int col=0; col<BOARD_SIZE;col++){
+                JButton button = new JButton("");
+                buttons[row][col]=button;
+                button.setFont(font);
+                button.addActionListener(this);
+                super.add(button);
+            }
+        }
+        super.setResizable(false);
+        super.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton clickedButton = (JButton)e.getSource();
+        makeMove(clickedButton);
+        GameStatus gs = this.getGameStatus();
+        if(gs==GameStatus.Incomplete){
+            return;
+        }
+        declareWinner(gs);
+
+        int choice = JOptionPane.showConfirmDialog(this, "Do you want to restart");
+        if(choice==JOptionPane.YES_OPTION){
+            for(int row=0; row<BOARD_SIZE; row++){
+                for(int col=0; col<BOARD_SIZE;col++){
+                    buttons[row][col].setText("");
+                }
+            }
+            CrossTurn=true;
+        }else{
+            super.dispose();
+        }
+    }
+    private void makeMove(JButton clickedButton){
+        String btntext = clickedButton.getText();
+        if(btntext.length()>0){
+            JOptionPane.showMessageDialog(this, "Invalid Move");
+        }
+        else{
+            if(CrossTurn){
+                clickedButton.setText("X");
+            }
+            else{
+                clickedButton.setText("O");
+            }
+            CrossTurn = !CrossTurn; 
+        }
+    }
+    private GameStatus getGameStatus(){
+        String text1 = "" , text2 = "";
+        int row=0;
+        int col=0;
+
+        row=0;
+        while (row<BOARD_SIZE) {
+            col=0;
+            while (col<BOARD_SIZE-1) {
+                text1=buttons[row][col].getText();
+                text2=buttons[row][col+1].getText();
+                if(!text1.equals(text2)||text1.length()==0){
+                    break;
+                }
+                col++;
+            }
+            if(col==BOARD_SIZE-1){
+                if(text1.equals("X")){
+                    return GameStatus.XWins;
+                }else{
+                    return GameStatus.OWins;
+                }
+            }
+            row++;
+        }
+        col=0;
+        while (col<BOARD_SIZE) {
+            row=0;
+            while (row<BOARD_SIZE-1) {
+                text1=buttons[row][col].getText();
+                text2=buttons[row+1][col].getText();
+                if(!text1.equals(text2)||text1.length()==0){
+                    break;
+                }
+                row++;
+            }
+            if(row==BOARD_SIZE-1){
+                if(text1.equals("X")){
+                    return GameStatus.XWins;
+                }else{
+                    return GameStatus.OWins;
+                }
+            }
+            col++;
+        }
+
+        row=0;
+        col=0;
+        while ((row<BOARD_SIZE-1)) {
+            text1=buttons[row][col].getText();
+            text2=buttons[row+1][col+1].getText();
+            if(!text1.equals(text2)||text1.length()==0){
+                break;
+            }
+            row++;
+            col++;
+        }
+        if(row==BOARD_SIZE-1){
+            if(text1.equals("X")){
+                return GameStatus.XWins;
+            }
+            else{
+                return GameStatus.OWins;
+            }
+        }
+        // 2nd
+        row=BOARD_SIZE-1;
+        col=0;
+        while ((row<0)) {
+            text1=buttons[row][col].getText();
+            text2=buttons[row-1][col+1].getText();
+            if(!text1.equals(text2)||text1.length()==0){
+                break;
+            }
+            row--;
+            col++;
+        }
+        if(row==0){
+            if(text1.equals("X")){
+                return GameStatus.XWins;
+            }
+            else{
+                return GameStatus.OWins;
+            }
+        }
+        String txt = "";
+        for(row=0; row<BOARD_SIZE; row++){
+            for(col=0; col<BOARD_SIZE; col++){
+                txt = buttons[row][col].getText();
+                if(txt.length()==0){
+                    return GameStatus.Incomplete;
+                }
+            }
+        }
+        return GameStatus.Tie;
+    }
+    private void declareWinner(GameStatus gs){
+        if(gs==GameStatus.XWins){
+            JOptionPane.showMessageDialog(this, "X Wins");
+        }else if(gs==GameStatus.OWins){
+            JOptionPane.showMessageDialog(this, "O Wins");
+        }else{
+            JOptionPane.showMessageDialog(this, "Tie");
+        }
+    }
+}
